f3f35650f29794771921f6cfc708059f862c6a4e
Chanin Timbal <chanin.n.timbal@gmail.com>
2025-06-27 19:51:15 -0700

    Starting Array ADT

--- DataStructures/DataStructures/array.hpp
+++ DataStructures/DataStructures/array.hpp
@@ -61,6 +61,151 @@ namespace data_structures
 		void Length(const size_t& length);
 		T* AllocateArray(const size_t& length);
 	};
+	template<class T>
+	inline Array<T>::~Array()
+	{
+		delete[] storage_;
+		storage_ = nullptr;
+	}
+	template<class T>
+	inline Array<T>::Array(size_t length) : length_(length)
+	{
+		T* storage = nullptr;
+		try
+		{
+			storage = new T[length]{};
+		}
+		catch (const std::bad_alloc& ex)
+		{
+			delete[] storage;
+			storage = nullptr;
+			throw AdtException(ex.what());
+		}
+		storage_ = storage;
+	}
+
+	template<class T>
+	inline Array<T>::Array(const T* storage, const size_t& length) : length_(length)
+	{
+		T* storage = nullptr;
+		try
+		{
+			storage = new T[length]{};
+		}
+		catch (const std::bad_alloc& ex)
+		{
+			delete[] storage;
+			storage = nullptr;
+			throw AdtException(ex.what());
+		}
+		storage_ = storage;
+
+		for (auto i = 0ull; i < length; ++i) {
+			storage_[i] = storage[i];
+		}
+	}
+	template<class T>
+	inline Array<T>::Array(const Array& copy)
+	{
+		*this = copy;
+	}
+
+	template<class T>
+	inline Array& Array<T>::operator=(const Array& rhs)
+	{
+		//myarray = someOtherArray
+
+	}
+	template<class T>
+	inline Array<T>::Array(Array&& copy) noexcept
+	{
+	}
+	template<class T>
+	inline Array& Array<T>::operator=(Array&& rhs) noexcept
+	{
+		// TODO: insert return statement here
+	}
+	template<class T>
+	inline T& Array<T>::operator[](const size_t& index)
+	{
+		// TODO: insert return statement here
+	}
+	template<class T>
+	inline const T& Array<T>::operator[](const size_t& index) const
+	{
+		// TODO: insert return statement here
+	}
+	template<class T>
+	inline Array<T>::operator bool() const noexcept
+	{
+	}
+	template<class T>
+	inline bool Array<T>::operator==(const Array& rhs) const noexcept
+	{
+		return false;
+	}
+	template<class T>
+	inline bool Array<T>::operator!=(const Array& rhs) const noexcept
+	{
+		return false;
+	}
+	template<class T>
+	inline size_t Array<T>::size() const noexcept
+	{
+		return size_t();
+	}
+	template<class T>
+	inline bool Array<T>::empty() const noexcept
+	{
+		return false;
+	}
+	template<class T>
+	inline void Array<T>::resize(size_t newSize)
+	{
+	}
+	template<class T>
+	inline void Array<T>::clear()
+	{
+	}
+	template<class T>
+	inline iterator Array<T>::begin() noexcept
+	{
+		return iterator();
+	}
+	template<class T>
+	inline const_iterator Array<T>::begin() const noexcept
+	{
+		return const_iterator();
+	}
+	template<class T>
+	inline const_iterator Array<T>::cbegin() const noexcept
+	{
+		return const_iterator();
+	}
+	template<class T>
+	inline iterator Array<T>::end() noexcept
+	{
+		return iterator();
+	}
+	template<class T>
+	inline const_iterator Array<T>::end() const noexcept
+	{
+		return const_iterator();
+	}
+	template<class T>
+	inline const_iterator Array<T>::cend() const noexcept
+	{
+		return const_iterator();
+	}
+	template<class T>
+	inline void Array<T>::Length(const size_t& length)
+	{
+	}
+	template<class T>
+	inline T* Array<T>::AllocateArray(const size_t& length)
+	{
+		return nullptr;
+	}
 }
 
 #endif
