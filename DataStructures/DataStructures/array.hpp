14cc20d935a4f057df64770eafd39be8ae6eedda
chanintimbal <chanin.timbal@oit.edu>
2025-07-03 17:21:44 -0700

    continued redoing array code

--- DataStructures/DataStructures/array.hpp
+++ DataStructures/DataStructures/array.hpp
@@ -72,25 +72,13 @@ namespace data_structures
 	template<class T>
 	inline Array<T>::Array(size_t length) : length_(length)
 	{
-
+		storage_ = AllocateArray(length);
 	}
 
 	template<class T>
 	inline Array<T>::Array(const T* storage, const size_t& length)
 	{
-		T* storage = nullptr;
-		try
-		{
-			storage = new T[length]{};
-		}
-		catch
-		{
-			delete[] stroage;
-			storage = nullptr;
-			throw AdtException(ex.what());
-		}
-
-		storage_ = storage;
+		storage_ = AllocateArray(length);
 
 		for (auto i = 0ull; i < length; ++i)
 		{
@@ -107,55 +95,111 @@ namespace data_structures
 	template<class T>
 	inline Array& Array<T>::operator=(const Array& rhs)
 	{
-		// TODO: insert return statement here
+		// myarray = someotherarray
+		if (this!=&rhs)
+		{
+			T* new_storage = AllocateArray(rhs.length_);
+
+			for (auto i = 0ull; i < rhs.length_; ++i)
+			{
+				new storage[i] = rhs.storage_[i];
+			}
+			delete[] storage_;
+			storage_ = new_storage;
+			length_ = rhs.length_;
+		}
+		return *this;
 	}
 	template<class T>
-	inline Array<T>::Array(Array&& copy) noexcept
+	inline Array<T>::Array(Array&& copy) noexcept : storage_(copy.storage_), length_(copy.length_)
 	{
+		copy.storage_ = nullptr;
+		copy.length_ = 0;
 	}
+
 	template<class T>
-	inline Array& Array<T>::operator=(Array&& rhs) noexcept
+	Array<T>& Array<T>::operator=(Array&& rhs) noexcept
 	{
-		// TODO: insert return statement here
+		if (this != &rhs)
+		{
+			delete[] storage_;
+			storage_ = nullptr;
+
+			storage_ = rhs.storage_;
+			length_ = rhs.length_;
+
+			rhs.storage_ = nullptr;
+			rhs.length_ = 0;
+		}
+		return *this;
 	}
+
 	template<class T>
 	inline T& Array<T>::operator[](const size_t& index)
 	{
-		// TODO: insert return statement here
+		if (index >= length_)
+		{
+			throw AdtException("Outside the bounds of the array");
+		}
+
+		return storage_[index];
 	}
+
 	template<class T>
 	inline const T& Array<T>::operator[](const size_t& index) const
 	{
-		// TODO: insert return statement here
+		if (index >= length_)
+		{
+			throw AdtException("Outside the bounds of the array");
+		}
+
+		return storage_[index];
 	}
+
 	template<class T>
 	inline Array<T>::operator bool() const noexcept
 	{
+		return storage_ != nullptr;
 	}
+
 	template<class T>
 	inline bool Array<T>::operator==(const Array& rhs) const noexcept
 	{
+		if (length_ == rhs.length_)
+		{
+			for (auto i = 0ull; i < length_; ++i)
+			{
+				if (storage_[i] == rhs.storage_[i])
+					return false;
+			}
+			return true;
+		}
 		return false;
 	}
+
 	template<class T>
 	inline bool Array<T>::operator!=(const Array& rhs) const noexcept
 	{
-		return false;
+		return !(*this == rhs);
 	}
+
 	template<class T>
 	inline size_t Array<T>::size() const noexcept
 	{
-		return size_t();
+		return length_;
 	}
+
 	template<class T>
 	inline bool Array<T>::empty() const noexcept
 	{
-		return false;
+		return length_ == 0;
 	}
+
 	template<class T>
 	inline void Array<T>::resize(size_t newSize)
 	{
 	}
+
 	template<class T>
 	inline void Array<T>::clear()
 	{
