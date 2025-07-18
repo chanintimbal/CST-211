0ab0dad5b6760892fa2b0f412dab7fac7c6e43f2
chanintimbal <chanin.timbal@oit.edu>
2025-07-17 17:48:01 -0700

    Array2D implemented fully,  Tests passed: OperatorEqualsAndNotEquals, ResizeRowd_ShrinkAndGrow_PreservesAndValueInits_SunnyAndRainy, SelectAndOpIndex_SunnyDay

--- DataStructures/DataStructures.Test/array2d_test.cpp
+++ DataStructures/DataStructures.Test/array2d_test.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-
+#include "Array2d.hpp"
 #include "crt_check_memory.hpp"
+
+using namespace data_structures;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,12 +11,97 @@ namespace data_structures_tests
 {
 	TEST_CLASS(Array2DTest)
 	{
-	public:
+		static Array2D<int> IntFactory(size_t rows = 4, size_t columns = 4)
+		{
+			Array2D<int> array(rows, columns);
+			int counter = 0;
 
-		TEST_METHOD(TestMethod1)
+			for (size_t i = 0; i < array.Rows(); ++i)
+			{
+				for (size_t j = 0; j < array.Columns(); ++j)
+					array[i][j] = counter++;
+			}
+
+			return array;
+		}
+
+
+	public:
+		TEST_METHOD(SelectAndOpIndex_SunnyDay)
 		{
 			const CrtCheckMemory check;
 
+			// Arrange
+			auto array = IntFactory();
+
+			int sumSelect = 0, sumIndex = 0;
+
+			// Act: sum via Select() and sum via operator[][]
+			for (size_t i = 0; i < 4ull; ++i)
+			{
+				for (size_t j = 0; j < 4ull; ++j)
+				{
+					sumSelect += array.Select(i, j);
+					sumIndex += array[i][j];
+				}
+			}
+
+			// Assert
+			Assert::AreEqual(120, sumSelect, L"Sum via Select()");
+			Assert::AreEqual(120, sumIndex, L"Sum via operator[][]");
+		}
+
+		TEST_METHOD(ResizeRowd_ShrinkAndGrow_PreservesAndValueInits_SunnyAndRainy)
+		{
+			const CrtCheckMemory check;
+
+			// Arrange
+			Array2D<int> array(2ull, 3ull);
+
+			// Fill
+			array[0][0] = 10; array[0][1] = 11; array[0][2] = 12;
+			array[1][0] = 20; array[1][1] = 21; array[1][2] = 22;
+
+			// Act1: shrink to 1 row
+			array.Rows(1ull);
+
+			// Assert
+			Assert::AreEqual(1ull, array.Rows(), L"Rows after shrink to 1");
+			Assert::AreEqual(3ull, array.Columns(), L"Columns remain 3");
+			Assert::AreEqual(10, array[0][0], L"Retained[0][0]");
+			Assert::AreEqual(11, array[0][1], L"Retained[0][1]");
+			Assert::AreEqual(12, array[0][2], L"Retained[0][2]");
+			Assert::ExpectException<AdtException>([&]() {array.Select(1ull, 0); });
+			Assert::ExpectException<AdtException>([&]() {array.Select(2ull, 0); });
+
+			// Act 2: Grow to 3 rows
+			array.Rows(3ull);
+
+			//Assert grow
+			Assert::AreEqual(3ull, array.Rows(), L"Rows after grow to 3");
+			Assert::AreEqual(10, array[0][0], L"Retained[0][0]");
+			Assert::AreEqual(11, array[0][1], L"Retained[0][1]");
+			Assert::AreEqual(12, array[0][2], L"Retained[0][2]");
+			Assert::AreEqual(0, array[1][0], L"New rows default to 0");
+			Assert::AreEqual(0, array[1][1], L"New rows default to 0");
+			Assert::AreEqual(0, array[1][2], L"New rows default to 0");
+			Assert::AreEqual(0, array[2][0], L"New rows default to 0");
+			Assert::AreEqual(0, array[2][1], L"New rows default to 0");
+			Assert::AreEqual(0, array[2][2], L"New rows default to 0");
+		}
+
+		TEST_METHOD(OperatorEqualsAndNotEquals)
+		{
+			const CrtCheckMemory check;
+
+			// Arrange
+			auto array1 = IntFactory();
+			auto array2 = IntFactory();
+
+			// Act and Assert
+			Assert::IsTrue(array1 == array2, L"Two identical factories compare equal");
+			array2[2][3] = -1;
+			Assert::IsTrue(array1 != array2, L"After mutation, the arrays differ");
 		}
 	};
 }
--- DataStructures/DataStructures/array2d.hpp
+++ DataStructures/DataStructures/array2d.hpp
@@ -44,5 +44,174 @@ namespace data_structures
 
 		static void CheckBounds(size_t row, size_t col, size_t rows, size_t cols);
 	};
+
+	template <typename T>
+	Array2D<T>::Array2D(size_t rows, size_t columns)
+	: storage_(rows*columns), rows_(rows), columns_(columns) {}
+
+	template <typename T>
+	Array2D<T>::Array2D(const Array2D& copy)
+		: storage_(std::move(copy.storage_)), rows_(copy.rows_), columns_(copy.columns_) {}
+
+	template <typename T>
+	Array2D<T>& Array2D<T>::operator=(const Array2D& copy)
+	{
+		if (this != f&copy)
+		{
+			rows_ = copy.rows_;
+			columns_ = copy.columns_;
+			storage_ = copy.storage_;
+		}
+		return *this;
+	}
+
+	template <typename T>
+	Array2D<T>::Array2D(Array2D&& copy) noexcept
+		: storage_(std::move(copy.storage_)), rows_(copy.rows_), columns_(copy.columns_)
+	{
+		copy.rows_ = copy.columns_ = 0;
+	}
+
+	template <typename T>
+	Array2D<T>& Array2D<T>::operator=(Array2D&& copy) noexcept
+	{
+		if (this!= &copy)
+		{
+			rows_ = copy.rows_;
+			columns_ = copy.columns_;
+			storage_ = std::move(copy.storage_);
+			copy.rows_ = copy.columns_ = 0;
+		}
+		return *this;
+	}
+
+	template <typename T>
+	T Array2D<T>::Select(size_t row, size_t column) const
+	{
+		CheckBounds(row, column, rows_, columns_);
+		return storage_[row * columns_ + column];
+	}
+
+	template <typename T>
+	T& Array2D<T>::Select(size_t row, size_t column)
+	{
+		CheckBounds(row, column, rows_, columns_);
+		return storage_[row * columns_ + column];
+	}
+
+	template <typename T>
+	Row<T> Array2D<T>::operator[](size_t row_index) const
+	{
+		if (row_index >= rows_) throw AdtException("Row index out of bounds.");
+		return Row<T>(*this, row_index);
+	}
+
+	template <typename T>
+	Row<T> Array2D<T>::operator[](size_t row_index)
+	{
+		if (row_index >= rows_) throw AdtException("Row index out of bounds.");
+		return Row<T>(*this, row_index);
+	}
+
+	template <typename T>
+	Array2D<T>::operator bool() const noexcept
+	{
+		if (rows_ == 0 || columns_ == 0) return false;
+		return static_cast<bool>(storage_);
+	}
+
+	template <typename T>
+	size_t Array2D<T>::Rows() const noexcept
+	{
+		return rows_;
+	}
+
+	template <typename T>
+	size_t Array2D<T>::Columns() const noexcept
+	{
+		return columns_;
+	}
+
+	template <typename T>
+	void Array2D<T>::Rows(const size_t& new_rows)
+	{
+		if (new_rows == 0)
+		{
+			rows_ = 0;
+			storage_.resize(0);
+			return;
+		}
+
+		// Reduction and grow
+		// copy the array to temp array, overwrite the original with copy
+
+		Array<T> temp_array(new_rows * columns_);
+
+		size_t some_t = std::min(rows_, new_rows) * columns_;
+
+		for (size_t i = 0; i < some_t; ++i)
+		{
+			temp_array[i] = storage_[i];
+		}
+
+		storage_ = std::move(temp_array);
+		rows_ = new_rows;
+	}
+
+	template <typename T>
+	void Array2D<T>::Columns(const size_t& new_columns)
+	{
+		if (new_columns == 0)
+		{
+			columns_ = 0;
+			storage_.resize(0);
+			return;
+		}
+
+		Array<T> temp_array(rows_ * new_columns);
+
+		size_t some_t = std::min(columns_, new_columns);
+
+		for (size_t row = 0; row < rows_; ++row)
+		{
+			for (size_t col = 0; col < some_t; ++col)
+			{
+				temp_array[row * new_columns + col] = storage_[row * columns_ + col];
+			}
+		}
+
+		storage_ = std::move(temp_array);
+		columns_ = new_columns;
+	}
+
+	template <typename T>
+	bool Array2D<T>::operator==(const Array2D& rhs) const noexcept
+	{
+		
+		if (rows_ != rhs.rows_ || columns_ != rhs.columns_)
+			return false;
+
+		// Compare
+		for (size_t i = 0; i < rows_ * columns_; ++i)
+		{
+			if (storage_[i] != rhs.storage_[i])
+				return false;
+		}
+
+		return true;
+	}
+
+	template <typename T>
+	bool Array2D<T>::operator!=(const Array2D& rhs) const noexcept
+	{
+		return !(*this == rhs);
+	}
+
+	template <typename T>
+	void Array2D<T>::CheckBounds(size_t row, size_t col, size_t rows, size_t cols)
+	{
+		if (row >= rows || col >= cols)
+			throw AdtException("Select() Out of bounds");
+	}
 }
 #endif
