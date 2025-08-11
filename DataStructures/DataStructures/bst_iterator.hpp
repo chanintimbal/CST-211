1aefdb73ccf2756c84e78351a58d04a12f0e9e3b
chanintimbal <chanin.timbal@oit.edu>
2025-08-10 18:20:30 -0700

    bst_tree completed and bst_tree_tests completed (#10)

--- DataStructures/DataStructures/bst_iterator.hpp
+++ DataStructures/DataStructures/bst_iterator.hpp
@@ -20,29 +20,107 @@ namespace data_structures
 		using typename Base::pointer;
 
 		BstIterator() = default;
-		explicit BstIterator(NodeT* n);
-		explicit BstIterator(const NodeT* n);
+		explicit BstIterator(NodeT* n) : node_(n) {}
+		explicit BstIterator(const NodeT* n) : node_(const_cast<NodeT*>(n)) {}
 
-		operator BstIterator<Key, Value, true, NodeT>() const;
+		operator BstIterator<Key, Value, true, NodeT>() const
+		{
+			return BstIterator<Key, Value, true, NodeT>(node_);
+		}
 
-		reference operator*() override;
-		reference operator*() const override;
-		pointer operator->() override;
-		pointer operator->() const override;
+		reference operator*() override
+		{
+			if (!node_) throw AdtException("Dereferencing null BST iterator");
+			return *node_;
+		}
 
-		Base& operator++() override;
-		Base& operator--() override;
+		reference operator*() const override
+		{
+			if (!node_) throw AdtException("Dereferencing null BST iterator");
+			return *node_;
+		}
 
-		bool operator==(const Base& rhs) const override;
-		bool operator!=(const Base& rhs) const override;
+		pointer operator->() override
+		{
+			if (!node_) throw AdtException("Dereferencing null BST iterator");
+			return node_;
+		}
+
+		pointer operator->() const override
+		{
+			if (!node_) throw AdtException("Dereferencing null BST iterator");
+			return node_;
+		}
+
+		Base& operator++() override
+		{
+			node_ = Next(node_);
+			return *this;
+		}
+
+		Base& operator--() override
+		{
+			node_ = Prev(node_);
+			return *this;
+		}
+
+		bool operator==(const Base& rhs) const override
+		{
+			auto other = dynamic_cast<const BstIterator*>(&rhs);
+			return other && node_ == other->node_;
+		}
+
+		bool operator!=(const Base& rhs) const override
+		{
+			return !(*this == rhs);
+		}
 
 	private:
-		NodeT* node_ {nullptr};
+		NodeT* node_{ nullptr };
 
-		static NodeT* Min(NodeT* node) noexcept;
-		static NodeT* Max(NodeT* node) noexcept;
-		static NodeT* Next(NodeT* node) noexcept;
-		static NodeT* Prev(NodeT* node) noexcept;
+		static NodeT* Min(NodeT* node) noexcept
+		{
+			if (!node) return nullptr;
+			while (node->Left()) node = node->Left();
+			return node;
+		}
+
+		static NodeT* Max(NodeT* node) noexcept
+		{
+			if (!node) return nullptr;
+			while (node->Right()) node = node->Right();
+			return node;
+		}
+
+		static NodeT* Next(NodeT* node) noexcept
+		{
+			if (!node) return nullptr;
+			if (node->Right())
+				return Min(node->Right());
+
+			NodeT* parent = node->Parent();
+			while (parent && node == parent->Right())
+			{
+				node = parent;
+				parent = parent->Parent();
+			}
+			return parent;
+		}
+
+		static NodeT* Prev(NodeT* node) noexcept
+		{
+			if (!node) return nullptr;
+			if (node->Left())
+				return Max(node->Left());
+
+			NodeT* parent = node->Parent();
+			while (parent && node == parent->Left())
+			{
+				node = parent;
+				parent = parent->Parent();
+			}
+			return parent;
+		}
 	};
 }
 #endif
--- DataStructures/DataStructures/bst_tree.hpp
+++ DataStructures/DataStructures/bst_tree.hpp
@@ -31,10 +31,8 @@ namespace data_structures
 		std::pair<V&, bool> InsertImpl(const K& key, const V& value);
 		void RemoveImpl(const K& key);
 
-		// Indexer
 		V& operator[](const K& key);
 
-		// Iterators - forward only
 		iterator begin() noexcept;
 		iterator end() noexcept;
 
@@ -50,6 +48,186 @@ namespace data_structures
 
 		V& Lookup(Node* cur, const K& key) const;
 		V* LookupPtr(Node* cur, const K& key) const;
-	};	
+	};
+
+	template <class K, class V>
+	BstTree<K, V>::~BstTree()
+	{
+		this->Clear();
+	}
+
+	template <class K, class V>
+	BstTree<K, V>::BstTree(const BstTree& copy)
+	{
+		this->Copy(copy.Root());
+	}
+
+	template <class K, class V>
+	BstTree<K, V>& BstTree<K, V>::operator=(const BstTree& rhs)
+	{
+		if (this != &rhs)
+		{
+			this->Clear();
+			this->Copy(rhs.Root());
+		}
+		return *this;
+	}
+
+	template <class K, class V>
+	BstTree<K, V>::BstTree(BstTree&& copy) noexcept
+	{
+		this->Root() = copy.Root();
+		copy.Root() = nullptr;
+	}
+
+	template <class K, class V>
+	BstTree<K, V>& BstTree<K, V>::operator=(BstTree&& rhs) noexcept
+	{
+		if (this != &rhs)
+		{
+			this->Clear();
+			std::swap(this->Root(), rhs.Root());
+		}
+		return *this;
+	}
+
+	template <class K, class V>
+	std::pair<V&, bool> BstTree<K, V>::InsertImpl(const K& key, const V& value)
+	{
+		return Insert(this->Root(), key, value, nullptr);
+	}
+
+	template <class K, class V>
+	void BstTree<K, V>::RemoveImpl(const K& key)
+	{
+		Remove(this->Root(), key);
+	}
+
+	template <class K, class V>
+	V& BstTree<K, V>::operator[](const K& key)
+	{
+		return Base::insert(key, V{}).first;
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::iterator BstTree<K, V>::begin() noexcept
+	{
+		return iterator(this->Min(this->Root()));
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::iterator BstTree<K, V>::end() noexcept
+	{
+		return iterator(nullptr);
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::const_iterator BstTree<K, V>::begin() const noexcept
+	{
+		return const_iterator(this->Min(this->Root()));
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::const_iterator BstTree<K, V>::end() const noexcept
+	{
+		return const_iterator(nullptr);
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::const_iterator BstTree<K, V>::cbegin() const noexcept
+	{
+		return const_iterator(this->Min(this->Root()));
+	}
+
+	template <class K, class V>
+	typename BstTree<K, V>::const_iterator BstTree<K, V>::cend() noexcept
+	{
+		return const_iterator(nullptr);
+	}
+
+	template <class K, class V>
+	std::pair<V&, bool> BstTree<K, V>::Insert(Node*& cur, const K& key, const V& value, Node* parent)
+	{
+		if (!cur)
+		{
+			cur = new Node(key, value);
+			cur->Parent(parent);
+			return { cur->Value(), true };
+		}
+		if (key < cur->Key())
+			return Insert(cur->Left(), key, value, cur);
+		else if (key > cur->Key())
+			return Insert(cur->Right(), key, value, cur);
+		else
+			return { cur->Value(), false };  // key exists
+	}
+
+	template <class K, class V>
+	void BstTree<K, V>::Remove(Node*& cur, const K& key)
+	{
+		if (!cur) return;
+
+		if (key < cur->Key())
+		{
+			Remove(cur->Left(), key);
+		}
+		else if (key > cur->Key())
+		{
+			Remove(cur->Right(), key);
+		}
+		else
+		{
+			if (!cur->Left() && !cur->Right())
+			{
+				delete cur;
+				cur = nullptr;
+			}
+			else if (!cur->Left())
+			{
+				Node* temp = cur;
+				cur = cur->Right();
+				cur->Parent(temp->Parent());
+				delete temp;
+			}
+			else if (!cur->Right())
+			{
+				Node* temp = cur;
+				cur = cur->Left();
+				cur->Parent(temp->Parent());
+				delete temp;
+			}
+			else
+			{
+				Node* successor = this->MinNode(cur->Right());
+				cur->Key(successor->Key());
+				cur->Value() = successor->Value();
+				Remove(cur->Right(), successor->Key());
+			}
+		}
+	}
+
+	template <class K, class V>
+	V& BstTree<K, V>::Lookup(Node* cur, const K& key) const
+	{
+		V* valPtr = LookupPtr(cur, key);
+		if (!valPtr) throw AdtException("Key not found");
+		return *valPtr;
+	}
+
+	template <class K, class V>
+	V* BstTree<K, V>::LookupPtr(Node* cur, const K& key) const
+	{
+		while (cur)
+		{
+			if (key < cur->Key())
+				cur = cur->Left();
+			else if (key > cur->Key())
+				cur = cur->Right();
+			else
+				return &cur->Value();
+		}
+		return nullptr;
+	}
 }
+
 #endif
