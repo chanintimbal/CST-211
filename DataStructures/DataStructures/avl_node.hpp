4031616ae42589860e2e3fb85b899eda22fb766e
chanintimbal <chanin.timbal@oit.edu>
2025-08-11 14:56:57 -0700

    avl node completed

--- DataStructures/DataStructures/avl_node.hpp
+++ DataStructures/DataStructures/avl_node.hpp
@@ -52,6 +52,125 @@ namespace data_structures
 		AvlNode* right_{ nullptr };
 		AvlNode* parent_{ nullptr };
 		AvlNodeBalanceFactor balance_factor_{ AvlNodeBalanceFactor::Equal };
-	};	
+	};
+
+	template <class K, class V>
+	AvlNode<K, V>::AvlNode(const K& key, const V& value, AvlNode* left, AvlNode* right)
+		: PairNode<k, v>(key, value), left_(left), right_(right)
+	{	}
+
+	template <class K, class V>
+	AvlNode<K, V>::AvlNode(const AvlNode& copy)
+		: PairNode<K, V>(copy), left_(copy.left), right_(copy.right), parent_(copy.parent), balance_factor_(copy.balance_factor_)
+	{	}
+
+	template <class K, class V>
+	AvlNode<K, V>& AvlNode<K, V>::operator=(const AvlNode& rhs)
+	{
+		if (this != &rhs)
+		{
+			PairNode<K, V>::operator=(rhs);
+			left_ = rhs.left;
+			right_ = rhs.left;
+			parent_ = rhs.parent_;
+			balance_factor_ = rhs.balance_factor_;
+		}
+		return *this;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>::AvlNode(AvlNode&& copy) noexcept
+		: PairNode<K,V>(std::move(copy)), left_(copy.left_), right_(copy.right_), parent_(copy.parent), balance_factor_(copy.balance_factor_)
+	{
+		copy.left_ = nullptr;
+		copy.right_ = nullptr;
+		copy.parent_ = nullptr;
+		copy.balance_factor_ = AvlNodeBalanceFactor::Equal;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>& AvlNode<K, V>::operator=(AvlNode&& rhs) noexcept
+	{
+		if (this != &rhs)
+		{
+			PairNode<K, V>::operator=(std::move(rhs));
+			left_ = rhs.left;
+			right_ = rhs.left;
+			parent_ = rhs.parent;
+			balance_factor_ = rhs.balance_factor_;
+
+			rhs.left_ = nullptr;
+			rhs.right_ = nullptr;
+			rhs.parent_ = nullptr;
+			rhs.balance_factor_ = AvlNodeBalanceFactor::Equal;
+		}
+		return *this;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>*& AvlNode<K, V>::Left() noexcept
+	{
+		return left_;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>*& AvlNode<K, V>::Right() noexcept
+	{
+		return right_;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>* AvlNode<K, V>::Left() const noexcept
+	{
+		return left_;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>* AvlNode<K, V>::Right() const noexcept
+	{
+		return right_;
+	}
+
+	template <class K, class V>
+	void AvlNode<K, V>::Left(AvlNode* left) noexcept
+	{
+		left_ = left;
+	}
+
+	template <class K, class V>
+	void AvlNode<K, V>::Right(AvlNode* right) noexcept
+	{
+		right_ = right;
+	}
+
+	template <class K, class V>
+	AvlNodeBalanceFactor AvlNode<K, V>::BalanceFactor() const noexcept
+	{
+		return balance_factor_;
+	}
+
+	template <class K, class V>
+	void AvlNode<K, V>::BalanceFactor(AvlNodeBalanceFactor bf) noexcept
+	{
+		balance_factor_ = bf;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>* AvlNode<K, V>::Parent() const noexcept
+	{
+		return parent_;
+	}
+
+	template <class K, class V>
+	AvlNode<K, V>*& AvlNode<K, V>::Parent() noexcept
+	{
+		return parent_;
+	}
+
+	template <class K, class V>
+	void AvlNode<K, V>::Parent(AvlNode* p) noexcept
+	{
+		parent_ = p;
+	}
 }
 #endif // AVL_NODE_HPP
--- DataStructures/DataStructures/avl_tree.hpp
+++ DataStructures/DataStructures/avl_tree.hpp
@@ -60,7 +60,116 @@ namespace data_structures
         Node* RotateRight(Node* node);
     };
 
-	
+
+    template <class K, class V>
+    AvlTree<K, V>::~AvlTree()
+    {
+    }
+
+    template <class K, class V>
+    AvlTree<K, V>::AvlTree(const AvlTree& copy)
+    {
+    }
+
+    template <class K, class V>
+    AvlTree<K, V>& AvlTree<K, V>::operator=(const AvlTree& rhs)
+    {
+    }
+
+    template <class K, class V>
+    AvlTree<K, V>::AvlTree(AvlTree&& copy) noexcept
+    {
+    }
+
+    template <class K, class V>
+    AvlTree<K, V>& AvlTree<K, V>::operator=(AvlTree&& rhs) noexcept
+    {
+    }
+
+    template <class K, class V>
+    V& AvlTree<K, V>::operator[](const K& key)
+    {
+    }
+
+    template <class K, class V>
+    std::pair<V&, bool> AvlTree<K, V>::InsertImpl(const K& key, const V& value)
+    {
+    }
+
+    template <class K, class V>
+    void AvlTree<K, V>::RemoveImpl(const K& key)
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::iterator AvlTree<K, V>::begin() noexcept
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::iterator AvlTree<K, V>::end() noexcept
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::const_iterator AvlTree<K, V>::begin() const noexcept
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::const_iterator AvlTree<K, V>::end() const noexcept
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::const_iterator AvlTree<K, V>::cbegin() const noexcept
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::const_iterator AvlTree<K, V>::cend() const noexcept
+    {
+    }
+
+    template <class K, class V>
+    std::pair<V&, bool> AvlTree<K, V>::Insert(Node*& node, const K& key, const V& value, bool& taller)
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::Node* AvlTree<K, V>::Remove(Node* node, const K& key, bool& shorter)
+    {
+    }
+
+    template <class K, class V>
+    void AvlTree<K, V>::LeftBalance(Node*& node, bool& taller)
+    {
+    }
+
+    template <class K, class V>
+    void AvlTree<K, V>::RightBalance(Node*& node, bool& taller)
+    {
+    }
+
+    template <class K, class V>
+    void AvlTree<K, V>::RemoveLeftBalance(Node*& node, bool& shorter)
+    {
+    }
+
+    template <class K, class V>
+    void AvlTree<K, V>::RemoveRightBalance(Node*& node, bool& shorter)
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::Node* AvlTree<K, V>::RotateLeft(Node* node)
+    {
+    }
+
+    template <class K, class V>
+    typename AvlTree<K, V>::Node* AvlTree<K, V>::RotateRight(Node* node)
+    {
+    }
 }
 
 
